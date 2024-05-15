324c78a171f447342115445016852a750111c8cd
Henard Gertzen <henardgertzen@gmail.com>
2024-05-16 01:51:31 +0200

    Stopped duplicate chats

--- src/components/list/chatList/addUser/addUser.jsx
+++ src/components/list/chatList/addUser/addUser.jsx
@@ -14,10 +14,10 @@ import {
 } from "firebase/firestore";
 import { useState } from "react";
 import { useUserStore } from "../../../../lib/userStore";
+import { toast } from "react-toastify";
 
 const AddUser = ({ onClose }) => {
   const [user, setUser] = useState(null);
-
   const { currentUser } = useUserStore();
 
 
@@ -34,7 +34,30 @@ const AddUser = ({ onClose }) => {
       const querySnapShot = await getDocs(q);
 
       if (!querySnapShot.empty) {
-        setUser(querySnapShot.docs[0].data());
+        const searchedUser = querySnapShot.docs[0].data();
+        if(searchedUser.id !== currentUser.id){
+          const existingChatRef1 = collection(db, "userchats", currentUser.id, "chats");
+          const existingChatQuery1 = query(existingChatRef1, where("recieverId", "==", searchedUser.id));
+
+          const existingChatRef2 = collection(db, "userchats", searchedUser.id, "chats");
+          const existingChatQuery2 = query(existingChatRef2, where("recieverId", "==", currentUser.id));
+
+          const [existChatSnapshot1, existingChatSnapshot2] = await Promise.all([
+            getDocs(existingChatQuery1),
+            getDocs(existingChatQuery2),
+          ]);
+
+          if(existChatSnapshot1.empty && existingChatQuery2.empty){
+            setUser(searchedUser);
+            toast.done(searchedUser.username + " added successfully!");
+          }
+          else{
+            toast.warn("Chat with " + searchedUser.username + " already exists!");
+          }
+        }      
+        else{
+          toast.warn("Cannot message yourself!");
+        }
       }
     } catch (err) {
       console.log(err);
@@ -88,7 +111,7 @@ const AddUser = ({ onClose }) => {
             <img src={user.avatar || "./avatar.png"} alt="" />
             <span>{user.username}</span>
           </div>
-          <button onClick={handleAdd}>Add User</button>
+          <button onClick={handleAdd} disabled={user.id === currentUser.id}>Add User</button>
         </div>
       )}
     </div>
